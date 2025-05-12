3e621128644fc17afb44fc7ab81a440c956a6c6a
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-12 23:54:54 +0000

    feat: implement database connection using mongoose

---
+++ src/db/connect.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_URI } from "../config/env.js"
+
+connectDB = async () => {
+    try {
+        const connectionInstant = await mongoose.connect(`${DB_URI}/quickformx`);
+        console.log("Database connected ", connectionInstant.connection.host);
+    } catch (error) {
+        console.log("Database connection error", error);
+        process.exit(1);
+    }
+}
+export default connectDB;
