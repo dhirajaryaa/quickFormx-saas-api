671976b7803471dfd53a74227f9cdc8d2d57f002
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-13 01:25:40 +0000

    feat[rate-limit]: add express-rate-limit middleware for API rate limiting

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from 'cors';
 import cookieParser from "cookie-parser";
 import { ORIGIN } from "./config/env.js";
+import rateLimit from "express-rate-limit";
 
 // initialize express app
 const app = express();
@@ -16,5 +17,21 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(express.static("./public"));
+
+// rate limiter
+const limiter = rateLimit({
+    max: 3,
+    windowMs: 60 * 1000,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: "Too many requests from this IP, please try again in a minute"
+});
+
+// api routes
+app.use('/api', limiter); //? rate limiter middlewares
+// default routes welcome page
+app.get('/', (req, res) => {
+    res.send('<h4>Welcome to AI Form Builder API gateway. <a href="https://github.com/dhirajaryaa/quickFormx-saas-api" >View Docs</h1>')
+});
 
 export default app;
