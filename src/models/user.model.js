876031867f7034ebc50cc199561fcaf6d19c47de
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-13 13:57:35 +0000

    feat[model]:model: add user model with password hashing and validation

---
+++ src/models/user.model.js
@@ -0,0 +1,80 @@
+import mongoose from 'mongoose'
+// social link schema
+const linkSchema = new mongoose.Schema(
+    {
+        url: {
+            type: String,
+            default: ''
+        },
+        title: {
+            type: String,
+            default: ''
+        }
+    },
+    { _id: false }
+)
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name is Required'],
+            trim: true
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is Required'],
+            trim: true,
+            lowercase: true,
+            unique: true
+        },
+        username: {
+            type: String,
+            required: [true, 'Username is Required'],
+            trim: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is Required']
+        },
+        forms: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Form'
+            }
+        ],
+        isPublic: {
+            type: Boolean,
+            default: false
+        },
+        age: Number,
+        address: String,
+        links: [linkSchema],
+        tagline: String,
+        avatar: {
+            url: {
+                type: String,
+                default: ''
+            },
+            publicId: {
+                type: String,
+                default: ''
+            }
+        }
+    },
+    { timestamps: true }
+)
+// password hash middleware
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next()
+    this.password = await bcrypt.hash(this.password, 12)
+    next()
+})
+// password match middleware
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+const userModel = mongoose.model('User', userSchema)
+export default userModel
