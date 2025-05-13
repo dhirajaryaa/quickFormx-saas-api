ac59fc9a905d81c9f3fd0084c922ccccb33bf6a2
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-13 00:21:03 +0000

    feat: setup middlewares and enhance dotenv configuration

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 import express from "express";
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+import { ORIGIN } from "./config/env.js";
+
 // initialize express app
 const app = express();
+
+// middlewares setup
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// cors setup
+app.use(cors({
+    origin: ORIGIN,
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(express.static("./public"));
 
 export default app;
