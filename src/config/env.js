d28d891e1c6822fce7bea9cd860b0c7c07ad445a
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-14 13:54:03 +0000

    feat(model): add JWT token generation methods for access and refresh tokens

--- src/config/env.js
+++ src/config/env.js
@@ -7,11 +7,19 @@ configDotenv({
 const config = Object.freeze({
     PORT: process.env.PORT,
     DB_URI: process.env.DB_URI,
-    ORIGIN: process.env.ORIGIN
+    ORIGIN: process.env.ORIGIN,
+    Access_Token_Secret: process.env.Access_Token_Secret,
+    Access_Token_Expired: process.env.Access_Token_Expired,
+    Refresh_Token_Secret: process.env.Refresh_Token_Secret,
+    Refresh_Token_Expired: process.env.Refresh_Token_Expired,
 });
 
 export const {
     PORT,
     DB_URI,
-    ORIGIN
+    ORIGIN,
+    Access_Token_Secret,
+    Access_Token_Expired,
+    Refresh_Token_Secret,
+    Refresh_Token_Expired
 } = config;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose';
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+import { Access_Token_Secret, Access_Token_Expired, Refresh_Token_Expired, Refresh_Token_Secret } from "../config/env.js"
+
 // social link schema
 const linkSchema = new mongoose.Schema(
     {
@@ -75,6 +78,24 @@ userSchema.pre('save', async function (next) {
 // password match middleware
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
+}
+// access Token generate middleware
+userSchema.methods.generateAccessToken = async function () {
+    return await jwt.sign({
+        _id: this._id,
+        email: this.email,
+        name: this.name
+    }, Access_Token_Secret, {
+        expiresIn: Access_Token_Expired
+    })
+}
+// refresh Token generate middleware
+userSchema.methods.generateRefreshToken = async function () {
+    return await jwt.sign({
+        _id: this._id
+    }, Refresh_Token_Secret, {
+        expiresIn: Refresh_Token_Expired
+    })
 }
 
 const userModel = mongoose.model('User', userSchema)
