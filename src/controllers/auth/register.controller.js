1a93337356a215f1a28fd3faf217d281cf64aead
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-14 00:56:16 +0000

    feat(auth): implement user registration with validation and error handling

--- src/controllers/auth/register.controller.js
+++ src/controllers/auth/register.controller.js
@@ -2,10 +2,35 @@ import AsyncHandler from "../../utils/asyncHandler.js";
 import ApiError from "../../utils/apiError.js";
 import ApiResponse from "../../utils/apiResponse.js";
 import userModel from "../../models/user.model.js"
+import registerSchema from "../../validators/auth/register.js"
+import ValidationError from "../../utils/validationError.js";
 
 const userRegister = AsyncHandler(async (req, res) => {
+    const { name, email, username, password } = req.body;
+    // check user input
+    const validate = registerSchema.safeParse(req.body)
+    if (!validate.success) {
+        const errorMessage = ValidationError(validate.error);
+        throw new ApiError(400, errorMessage)
+
+    };
+    // check user exist or not
+    const userExits = await userModel.findOne({
+        $or: [{ email }, { username }]
+    });
+    if (userExits) {
+        throw new ApiError(404, "user already exists")
+    };
+    const newUser = await userModel.create({
+        name,
+        email,
+        password,
+        username
+    });
+    const userDetails = await userModel.findById(newUser._id).select("-password")
+
     return res
         .status(201)
-        .json(new ApiResponse(201, "User Register successful!", {}));
+        .json(new ApiResponse(201, "User Register successful!", userDetails));
 })
 export default userRegister;
--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import userRegister from "../controllers/auth/register.controller.js";
+
 const authRouter = Router();
 
 // register user
-authRouter.post("/",userRegister)
+authRouter.post("/register", userRegister);
 
 export default authRouter;
