185bee325f0cf2f03825091937b0c83654da3987
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-15 00:32:16 +0000

    feat(auth): add email verification token generation and sending during user registration

--- src/controllers/auth/register.controller.js
+++ src/controllers/auth/register.controller.js
@@ -4,6 +4,9 @@ import ApiResponse from "../../utils/apiResponse.js";
 import userModel from "../../models/user.model.js"
 import registerSchema from "../../validators/auth/register.js"
 import ValidationError from "../../utils/validationError.js";
+import crypto from 'node:crypto'
+import sendEmail from "../../utils/sendEmail.js";
+import { URL } from '../../config/env.js'
 
 const userRegister = AsyncHandler(async (req, res) => {
     const { name, email, username, password } = req.body;
@@ -23,13 +26,23 @@ const userRegister = AsyncHandler(async (req, res) => {
     }
     if (userExits) {
     };
+    // generate email token
+    const token = crypto.randomBytes(32).toString('hex');
+    //  create user
     const newUser = await userModel.create({
         name,
         email,
         password,
-        username
+        username,
+        verificationToken: token
     });
-    const userDetails = await userModel.findById(newUser._id).select("-password")
+    // send verification email
+    await sendEmail({
+        to: newUser.name,
+        subject: "Verification Email from QucikFormx",
+        username: newUser.name, link: `${URL}/verify-account?token=${token}`
+    });
+    const userDetails = await userModel.findById(newUser._id).select("-password -refreshToken -isVerified -googleId -verificationToken")
 
     return res
         .status(201)
