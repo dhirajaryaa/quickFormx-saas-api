68c99448edf58708c1f331ca2905db4ac6b2d47a
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-15 15:07:13 +0000

    feat(form): add form model with field schema and user reference

---
+++ src/models/form.model.js
@@ -0,0 +1,60 @@
+import mongoose, { Schema } from "mongoose";
+
+const fieldSchema = new Schema({
+    label: String,
+    name: String,
+    required: {
+        type: Boolean,
+        default: true
+    },
+    type: {
+        type: String,
+        enum: ["text", "textarea", "email", "number", "radio", "checkbox", "select", "date", "file"],
+        required: true
+    },
+    placeholder: { type: String },
+    options: [{
+        value: String,
+        checked: {
+            type: Boolean,
+            default: false
+        }
+    }],  // for select , checkbox , radio button
+}, { _id: false });
+
+const formSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    authUser: {
+        type: Boolean,
+        default: false
+    },
+    publicUrl: {
+        type: String,
+        default: ""
+    },
+    proForm: {
+        type: Boolean,
+        default: false
+    },
+    branding: {
+        type: String,
+        default: "QuickFormX"
+    },
+    isDraft: {
+        type: Boolean,
+        default: true
+    },
+    fields: [fieldSchema],
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true });
+
+export const Form = mongoose.model("Form", formSchema);
