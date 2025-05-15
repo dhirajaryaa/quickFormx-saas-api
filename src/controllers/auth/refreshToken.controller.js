7017de81172cae0e5c81ab1c51576e624bc0f5e7
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-15 13:08:05 +0000

    feat(auth): implement account verification and access token refresh functionality

---
+++ src/controllers/auth/refreshToken.controller.js
@@ -0,0 +1,21 @@
+import AsyncHandler from "../../utils/asyncHandler.js";
+import ApiError from "../../utils/apiError.js";
+import ApiResponse from "../../utils/apiResponse.js";
+import userModel from "../../models/user.model.js"
+import { cookiesOptions } from "../../config/env.js"
+import { generateAccessAndRefreshToken } from './login.controller.js'
+
+const refreshAccessToken = AsyncHandler(async (req, res) => {
+    if (req.user) {
+        throw new ApiError(400, 'unauthorized or invalid action');
+    }
+    const user = await userModel.findById(req?.user._id);
+    // generate token
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user);
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, cookiesOptions)
+        .cookie("refreshToken", refreshToken, cookiesOptions)
+        .json(new ApiResponse(200, "AccessToken refresh successful!", { accessToken, refreshToken }));
+});
+export default refreshAccessToken;
