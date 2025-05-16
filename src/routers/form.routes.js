1164bb54980a01567b3419236833cc29f37099d3
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-16 01:17:04 +0000

    feat(form): add getAllForms controller and route for fetching all user forms with pagination

--- src/routers/form.routes.js
+++ src/routers/form.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authorizedRoutes from "../middlewares/auth.middleware.js";
 import createNewForm from "../controllers/form/createForm.controller.js";
 import getForm from "../controllers/form/getForm.controller.js";
+import getAllForms from "../controllers/form/getForms.controller.js";
 
 const formRouter = Router();
 
@@ -10,5 +11,7 @@ const formRouter = Router();
 formRouter.post("/",authorizedRoutes,createNewForm);
 //get form
 formRouter.get("/:formId",authorizedRoutes,getForm);
+//get forms
+formRouter.get("/",authorizedRoutes,getAllForms);
 
 export default formRouter;
