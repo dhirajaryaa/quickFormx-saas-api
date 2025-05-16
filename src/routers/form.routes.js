0254eed610150b114885b90138e84ad4d3337478
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-16 00:42:08 +0000

    feat(form): implement getForm controller and add route for fetching forms

--- src/routers/form.routes.js
+++ src/routers/form.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import authorizedRoutes from "../middlewares/auth.middleware.js";
 import createNewForm from "../controllers/form/createForm.controller.js";
+import getForm from "../controllers/form/getForm.controller.js";
 
 const formRouter = Router();
 
 //? form routes
 //create
 formRouter.post("/",authorizedRoutes,createNewForm);
+//get form
+formRouter.get("/:formId",authorizedRoutes,getForm);
 
 export default formRouter;
