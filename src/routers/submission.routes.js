73fa0a998127c5c21cdb3cc1cd725dab510f93ec
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-05-18 05:34:00 +0000

    feat(submission): implement getAllSubmissions controller and route for fetching submissions

--- src/routers/submission.routes.js
+++ src/routers/submission.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import authorizedRoutes from "../middlewares/auth.middleware.js";
 import saveNewSubmission from "../controllers/submission/create.controller.js";
+import getAllSubmissions from "../controllers/submission/getAllSubmission.controller.js";
 
 const submissionRouter = Router();
 
 // create new submission
 submissionRouter.post("/",saveNewSubmission);
+// get submissions
+submissionRouter.get("/responses",authorizedRoutes,getAllSubmissions);
 
 export default submissionRouter;
